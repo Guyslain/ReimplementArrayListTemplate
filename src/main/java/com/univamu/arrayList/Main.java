d56ea0145206e1f085b277b5134acd7ef6726931
guyslain.naves <guyslain.naves@lis-lab.fr>
2020-10-15 14:03:33 +0200

    Ajout d'une application Main

---
+++ src/main/java/com/univamu/arrayList/Main.java
@@ -0,0 +1,8 @@
+package com.univamu.arrayList;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Vous pouvez utiliser cette classe pour essayer vos méthodes");
+  }
+}
